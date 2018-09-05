46392150452b3d34aa5b54dca2d3c18d51aae65b
Melody Sue <melody@science37.com>
2018-09-05 11:51:57 -0700

    Added main file and gitignore

---
+++ main.py
@@ -0,0 +1,9 @@
+from yahoofinancials import YahooFinancials
+
+ticker = 'AMZN'
+yahoo_financials = YahooFinancials(ticker)
+
+balance_sheet_data_qt = yahoo_financials.get_financial_stmts('quarterly', 'balance')
+
+for item in balance_sheet_data_qt['balanceSheetHistoryQuarterly']['AMZN']:
+    print(item.keys())
